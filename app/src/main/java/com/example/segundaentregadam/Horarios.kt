f4182c33043fc538bb6a2590487ce33d28615809
Jonatan Alexis Rodriguez <jonatanr72@gmail.com>
2025-05-17 17:20:21 -0300

    Actividades lista, Horarios casi lista

--- app/src/main/java/com/example/segundaentregadam/Horarios.kt
+++ app/src/main/java/com/example/segundaentregadam/Horarios.kt
@@ -1,36 +1,35 @@
 package com.example.segundaentregadam
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class Actividades : AppCompatActivity() {
+class Horarios : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_actividades)
+        setContentView(R.layout.activity_horarios)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val elegirActividad = findViewById<CardView>(R.id.cardActividades1)
-        elegirActividad.setOnClickListener {
-            val intento = Intent(this, Horarios::class.java)
+        val boton = findViewById<Button>(R.id.btnVolver)
+        boton.setOnClickListener {
+            val intento = Intent(this, Actividades::class.java)
             startActivity(intento)
         }
 
-        val boton = findViewById<Button>(R.id.btnIrPrincipal2)
-        boton.setOnClickListener {
-            val intento = Intent(this, principal::class.java)
+        val elegirActividad = findViewById<TextView>(R.id.txtListaDeHorarios)
+        elegirActividad.setOnClickListener {
+            val intento = Intent(this, Actividades::class.java)
             startActivity(intento)
         }
     }
