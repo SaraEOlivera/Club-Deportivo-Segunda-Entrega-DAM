4614540dcfb88f72f88266d4d77d5ece21fe2133
SaraEOlivera <saraeolivera@gmail.com>
2025-05-30 11:50:17 -0300

    Se realiza conexion a BD

---
+++ app/src/main/java/UserDBHelper.kt
@@ -0,0 +1,33 @@
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class UserDBHelper(context: Context):SQLiteOpenHelper(context, "ClubDB", null, 1) {
+    //onCreate
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("""
+            CREATE TABLE usuarios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT UNIQUE,
+                clave TEXT
+            )
+        """.trimIndent())
+
+        db.execSQL("INSERT INTO usuarios(nombre, clave) VALUES ('admin', '1234')")
+        db.execSQL("INSERT INTO usuarios(nombre, clave) VALUES ('sara', 'sara2025')")
+    }
+    //onUpgrade
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun login (nombre:String, clave:String):Boolean{
+        val db = readableDatabase
+        val cursor = db.rawQuery(
+            "SELECT * FROM usuarios WHERE nombre =? AND clave=?",
+            arrayOf(nombre, clave)
+        )
+        val existe =cursor.count > 0
+        return  existe
+    }
+}
--- app/src/main/java/com/example/segundaentregadam/Login.kt
+++ app/src/main/java/com/example/segundaentregadam/Login.kt
@@ -1,12 +1,12 @@
 package com.example.segundaentregadam
 
+import UserDBHelper
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,13 +14,25 @@ class Login : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
-
+        val dbHelper = UserDBHelper(this)
+        val user = findViewById<EditText>(R.id.editTextUsuario)
+        val pass = findViewById<EditText>(R.id.editTextPassword)
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         val btnRegistrarseAdmin = findViewById<Button>(R.id.btnRegistrarse)
 
         btnLogin.setOnClickListener(){
-            val intentar = Intent(this, principal::class.java)
-            startActivity(intentar);
+            val userString = user.text.toString().trim()
+            val passString = pass.text.toString().trim()
+
+            if(dbHelper.login(userString, passString)){
+                val intentar = Intent(this, principal::class.java)
+                startActivity(intentar);
+            } else
+            {
+                val intentarError = Intent(this, FeedbackLoginError::class.java)
+                startActivity(intentarError);
+            }
+
         }
 
         btnRegistrarseAdmin.setOnClickListener(){
