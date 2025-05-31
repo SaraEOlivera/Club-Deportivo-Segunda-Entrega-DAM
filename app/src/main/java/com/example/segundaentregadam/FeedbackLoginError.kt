1567ab7bc7a9f4966ef586a8e6d884f106f7659d
SaraEOlivera <saraeolivera@gmail.com>
2025-05-30 21:04:57 -0300

    Intent feedback error

--- app/src/main/java/com/example/segundaentregadam/FeedbackLoginError.kt
+++ app/src/main/java/com/example/segundaentregadam/FeedbackLoginError.kt
@@ -1,11 +1,23 @@
 package com.example.segundaentregadam
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class FeedbackLoginError : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feedback_login_error)
+
+        val btnFbLogin = findViewById<Button>(R.id.btnVolverIntentar)
+        btnFbLogin.setOnClickListener(){
+            val intFbErrorLogin = Intent(this, Login::class.java)
+            startActivity(intFbErrorLogin)
+        }
+
+
+
+
     }
 }
